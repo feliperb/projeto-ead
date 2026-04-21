53a35b61643e0a27a6571cf0e7b6d97ebbcee511
Felipe Ribeiro <feliperibeirobarros@gmail.com>
2026-04-21 15:55:50 -0300

    Aplicando Cross Origin (CORS)

--- authuser/src/main/java/com/ead/authuser/config/ResolverConfig.java
+++ authuser/src/main/java/com/ead/authuser/config/ResolverConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +17,12 @@ public class ResolverConfig implements WebMvcConfigurer {
         PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
         resolver.setFallbackPageable(PageRequest.of(0, 10)); // Define a página padrão como 0 e o tamanho como 10
         resolvers.add(resolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/users/**").allowedOrigins("http://example.com").maxAge(3600);
+//        registry.addMapping("/auth/**").allowedOrigins("http://abc.com").maxAge(3600);
+        registry.addMapping("/**").maxAge(3600);
     }
 }
