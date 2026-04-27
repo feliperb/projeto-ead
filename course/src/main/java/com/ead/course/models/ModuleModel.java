a70625a8c272eb15b478500a1e46ad5d191f81bd
Felipe Ribeiro <feliperibeirobarros@gmail.com>
2026-04-27 14:52:09 -0300

    Criando Modulo Courses

---
+++ course/src/main/java/com/ead/course/models/ModuleModel.java
@@ -0,0 +1,35 @@
+package com.ead.course.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Entity
+@Table(name = "TB_MODULES")
+public class ModuleModel implements Serializable {  //Cada MODULE tem varias licoes
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID moduleId;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+
+}
