a41a1df0877f07a5d9fc0194aa443b4e782221c5
Felipe Ribeiro <feliperibeirobarros@gmail.com>
2026-05-11 16:55:56 -0300

    Add Logs com SLF

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -6,18 +6,19 @@ import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
+
+    Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 
     final UserService userService;
 
@@ -28,6 +29,16 @@ public class AuthenticationController {
                                                       @Valid
                                                       UserRecordDto userRecordDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(userRecordDto));
+    }
+
+    @GetMapping("/logs")
+    public String index() {
+        logger.trace("TRACE");
+        logger.debug("DEBUG");
+        logger.info("INFO");
+        logger.warn("WARN");
+        logger.error("ERROR");
+        return "Logging Spring Boot...";
     }
 
 }
